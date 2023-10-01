26bf2868eb73559e0c6c70b2b49de475495338d4
xee95 <zeeshannawaz2017@gmail.com>
2023-10-01 20:44:44 +0500

    dashboard initiated

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -17,12 +17,7 @@ const SetupPage =() => {
     }
   }, [isOpen, onOpen]);
 
-  return (
-    <div className='p-4'>
-      Root page
-    
-    </div>
-  )
+  return null;
 }
 
 export default SetupPage;
