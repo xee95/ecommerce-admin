19d5fedf9a9526a2b6b081ffed8a04966466fb92
xee95 <zeeshannawaz2017@gmail.com>
2023-10-04 20:49:45 +0500

    billboard completed

--- app/(dashboard)/[storeId]/(routes)/billboards/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/page.tsx
@@ -1,7 +1,37 @@
-const Billboards = () => {
-    return ( <>
-    <p>Billboards</p>
-    </> );
-}
- 
-export default Billboards;
+
+
+import prismadb from "@/lib/prismadb";
+import { format } from "date-fns";
+
+import { BillboardClient } from "./components/client";
+import { BillboardColumn } from "./components/column";
+
+const BillboardsPage = async ({
+  params
+}: {
+  params: { storeId: string }
+}) => {
+  const billboards = await prismadb.billboard.findMany({
+    where: {
+      storeId: params.storeId
+    },
+    orderBy: {
+      createdAt: 'desc'
+    }
+  }); 
+  const formattedBillboards: BillboardColumn[] = billboards.map((item) => ({
+    id: item.id,
+    label: item.label,
+    createdAt: format(item.createdAt, 'MMMM do, yyyy'),
+  }));
+
+  return (
+    <div className="flex-col">
+      <div className="flex-1 space-y-4 p-8 pt-6">
+        <BillboardClient data={formattedBillboards} />
+      </div>
+    </div>
+  );
+};
+
+export default BillboardsPage;
